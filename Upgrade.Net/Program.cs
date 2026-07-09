3691227a528d096a3cbfbd51913d6be83ef5bd09
avenyao <361341288@qq.com>
2026-07-09 16:09:49 +0800

    更新程序完善，添加命令行支持。

---
+++ Upgrade.Net/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Com.Scm.Upgrade
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Upgrade.Wpf/Dto/ScmAppInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace Com.Scm.Upgrade.Dto
+{
+    public class ScmAppInfo
+    {
+        public int types { get; set; }
+
+        /// <summary>
+        /// 应用代码
+        /// </summary>
+        public string code { get; set; }
+
+        /// <summary>
+        /// 应用名称
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// 应用介绍
+        /// </summary>
+        public string content { get; set; }
+    }
+}
